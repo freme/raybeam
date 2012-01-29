b669233764c93970e905279cc94573e35fd62688
Jiri Kraml <jiri.kraml@mailbox.tu-dresden.de>
2012-01-29 11:19:27 +0000

    proper clamping on image

--- image.cc
+++ image.cc
@@ -78,13 +78,10 @@ void Image::writePPM(ostream& out)
     for (i = ny-1; i >= 0; i--)
         for (j = 0; j < nx; j++)
         {
-            ired = (unsigned int) (256*raster[j][i].r());
-            igreen = (unsigned int) (256*raster[j][i].g());
-            iblue = (unsigned int) (256*raster[j][i].b());
-            // clamp if needed
-            if (ired > 255) ired = 255;
-            if (igreen > 255) igreen = 255;
-            if (iblue > 255) iblue = 255;
+            raster[j][i].clamp();
+            ired = (unsigned int) (255*raster[j][i].r());
+            igreen = (unsigned int) (255*raster[j][i].g());
+            iblue = (unsigned int) (255*raster[j][i].b());
             red = (unsigned char) (ired);
             green = (unsigned char) (igreen);
             blue = (unsigned char) (iblue);
