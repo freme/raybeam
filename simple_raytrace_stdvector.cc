a753395fc74dfcd7e5d5f823951933da5c744ba5
freme <freme@web.de>
2012-09-21 12:15:11 +0200

    switched to DynArray but kept old version using standard vectors

--- simple_raytrace_stdvector.cc
+++ simple_raytrace_stdvector.cc
@@ -1,5 +1,6 @@
 // simple_raytrace.cc
 
+#include <vector>
 #include <iostream>
 #include "vector3.h"
 #include "rgb.h"
@@ -7,35 +8,27 @@
 #include "shape.h"
 #include "triangle.h"
 #include "sphere.h"
-#include "dynarray.h"
-
 using namespace std;
 
 int main() {
     HitRecord rec;
     bool is_a_hit;
-    bool ret;
     float tmax;
-    unsigned static const int width = 1024;
-    unsigned static const int height = 768;
     Vector3 dir(0,0,-1); // direction of viewing rays
 
     // scene geometry
-    DynArray<Shape*> shapes;
-
-    shapes.append(new Sphere(Vector3(250,250,-1000), 150, rgb(.2, .2, .8)));
-    shapes.append(new Triangle(
+    vector<Shape*> shapes;
+    shapes.push_back(new Sphere(Vector3(250,250,-1000), 150, rgb(.2, .2, .8)));
+    shapes.push_back(new Triangle(
                 Vector3(300, 600, -800),
                 Vector3(0, 100, -1000),
                 Vector3(450, 20, -1000),
                 rgb(.8, .2, .2)));
-
-    shapes.truncate();
-    Image im(width, height, rgb(1, 0, 0));
+    Image im(10240, 7680);
 
     // loop over pixels
-    for (int i = 0; i < width; i++)
-        for (int j = 0; j < height; j++) {
+    for (int i = 0; i < 1024; i++)
+        for (int j = 0; j < 768; j++) {
             tmax = 100000.0f;
             is_a_hit = false;
             Ray r(Vector3(i,j,0), dir);
@@ -46,10 +39,22 @@ int main() {
                     tmax = rec.t;
                     is_a_hit = true;
                 }
-            if (is_a_hit) ret=im.set(i, j, rec.color);
-            else ret=im.set(i, j, rgb(.2, .2, .2));
-            if ( ! ret ) std::cerr << "setting pixel " << i << "x" << j << " failed" << std::endl;
+            if (is_a_hit) im.set(i, j, rec.color);
+            else im.set(i, j, rgb(.2, .2, .2));
         }
     im.writePPM(cout);
+
+// testing
+/*
+    Image * pic = new Image(1024, 768, rgb(1, 0, 0));
+    for (int i = 0; i < 1024; i++) {
+        for (int j = 0; j < 768; j++) {
+            ret = pic->set(i,j, rgb(0, 1, 0.5));
+            if ( ! ret ) std::cerr << "setting pixel " << i << "x" << j << " failed" << std::endl;
+        }
+    }
+    pic->writePPM(cout);
+*/
 }
+
 
